aa7967164e7ad0f1788c98e18f42b128fd38efa0
Edilnyc <Imanbaev.msc@gmail.com>
2022-05-23 21:09:15 +0600

    zm

---
+++ main.js
@@ -0,0 +1,52 @@
+// преоброзование 
+// let str = 'text'
+// let num = Boolean(10)
+// let bool = true 
+// let nul = String(null) 
+// let undef = "undefined "
+// let obj = `{}`
+// let symb = '$'
+// console.log(typeof(str))
+// console.log(typeof(num))
+// console.log(typeof(bool))
+// console.log(typeof(nul))
+// console.log(typeof(undef))
+// console.log(typeof(obj))
+// console.log(typeof(symb))
+// метод typeof() - помогает вам посмотреть тип данных
+
+// Условия ============
+// if(проверка){
+// выполняемое тело
+// }else if(следующая проверка){ а если //второй по приоритету
+//     выполняемое тело
+// }else{
+//     выполняемое тело
+// }
+
+
+// Операторы логические
+// && - и and
+// || - или  or 
+// % - деление без остатка
+
+// let user = prompt('Enter your name')
+// if(user == 'Dastan' || user == 'dastan'){
+//     alert('Welcome, Dastan!')
+// }else if(user == 'Дастан' || user == 'дастан'){
+//     alert('Привет, Дастан!')
+// }else{
+//     alert('Sorry, not for you')
+// }
+
+// let user = prompt('how old are you?')
+// if(user <18 || user >90 ){
+//     alert('sorry, not for you!')
+// }else{
+//     alert('welcome')
+// }
+
+// let user = +prompt('Введите 1-ое число?')
+// let user1 = +prompt('Введите 2-ое число?')
+// let user2 = +prompt('Введите 3-ое число?')
+// if
